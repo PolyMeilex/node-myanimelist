5d1d5a23eed08bfb3f21fa91e8c73b0cff30891f
Abady <gamersspeaks@gmail.com>
2018-12-05 19:38:51 +0300

    fix: Jikan V3 searching

--- methods/jikan/search.js
+++ methods/jikan/search.js
@@ -1,14 +1,17 @@
-module.exports = function (type, query, page) {
+module.exports = function (type, query, page, limit) {
     var rp = require('request-promise-native');
     var queryS = '';
     var pageS = '';
+    var limitS = '';
     if (query) {
         queryS = '/' + query;
         if (page)
-            pageS = '/' + page;
+            pageS = '&page=' + page;
+        if (limit)
+            limitS = '&limit=' + limit;
     }
     return new Promise(function (res, rej) {
-        rp(global['jikanBaseUrl'] + ("/search/" + type + queryS + pageS))
+        rp(global['jikanBaseUrl'] + ("/search/" + type + "?q=" + queryS + pageS + limitS))
             .then(function (res) { return JSON.parse(res); })
             .then(function (json) { return res(json); })
             .catch(function (err) { return rej("Error"); });
--- methods/jikan/search.ts
+++ methods/jikan/search.ts
@@ -1,15 +1,17 @@
-module.exports = (type:String,query:String,page:Number) => {
+module.exports = (type:String,query:String,page:Number,limit:Number) => {
     const rp = require('request-promise-native');
     let queryS:String = '';
     let pageS:String = '';
+    let limitS:String = '';
 
     if(query) {
         queryS = '/' + query;
-        if(page) pageS = '/' + page;
+        if(page) pageS = '&page=' + page;
+        if(limit) limitS = '&limit=' + limit
     }
 
     return new Promise( (res, rej) => {        
-        rp(global['jikanBaseUrl']+`/search/${type}${queryS}${pageS}`)
+        rp(global['jikanBaseUrl']+`/search/${type}?q=${queryS}${pageS}`)
         .then( res => JSON.parse(res) )
         .then( json => res(json) )
         .catch( err => rej("Error") )
