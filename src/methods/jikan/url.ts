22ca080b3ba0cffcdc6b13dfe16b6a1192ca8f37
Poly <bartek.marynczak@op.pl>
2019-09-01 21:20:55 +0200

    remove comment

--- src/methods/jikan/url.ts
+++ src/methods/jikan/url.ts
@@ -1,25 +1,5 @@
 import * as urljoin from "url-join";
 
-// class Url {
-//   baseUrl: string;
-//   parameters: string[];
-//   constructor(baseUrl: string) {
-//     this.baseUrl = baseUrl;
-//     this.parameters = [];
-//   }
-
-//   setParam(id: number, p: string) {
-//     this.parameters[id] = p;
-//   }
-
-//   getFullUrl(): string {
-//     let full = [...this.parameters];
-//     full.unshift(this.baseUrl);
-
-//     return urljoin(...full);
-//   }
-// }
-
 function joinUrl(base: string, params: string[]): string {
   return urljoin(base, ...params);
 }
