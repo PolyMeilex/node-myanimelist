daa8e7d6294df68cd078d805bd45efc43bce96a7
Poly <marynczak.bartlomiej@gmail.com>
2020-06-01 17:29:50 +0200

    Genre endpoint type definitions

--- src/methods/jikan/genre.ts
+++ src/methods/jikan/genre.ts
@@ -1,20 +1,22 @@
 import { joinUrl } from "./url";
-import { jikanGet, jikanUrl } from "./jikanApi";
+import { jikanGet, jikanUrl, Req } from "./jikanApi";
 
-import AnimeGenre from "./types/animeGenre";
-import MangaGenre from "./types/mangaGenre";
+import AnimeGenreId from "./types/animeGenre";
+import MangaGenreId from "./types/mangaGenre";
+
+import { AnimeGenre, MangaGenre } from "./types/genre";
 
 export class Genre {
   private baseUrl: string;
   constructor() {
     this.baseUrl = `${jikanUrl}/genre`;
   }
-  anime(genre: AnimeGenre, p?: number) {
+  anime(genre: AnimeGenreId, p?: number): Req<AnimeGenre> {
     let params: string[] = ["anime", String(genre)];
     if (p != null) params[2] = String(p);
     return jikanGet(joinUrl(this.baseUrl, params));
   }
-  manga(genre: MangaGenre, p?: number) {
+  manga(genre: MangaGenreId, p?: number): Req<MangaGenre> {
     let params: string[] = ["manga", String(genre)];
     if (p != null) params[2] = String(p);
     return jikanGet(joinUrl(this.baseUrl, params));
