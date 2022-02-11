a16b85fbf162c1c254b6c4e110ce1b64fd7be2e8
Poly <marynczakbartlomiej@gmail.com>
2022-02-12 00:41:42 +0100

    jikan4: Characters endpoint

---
+++ src/methods/jikan4/character.ts
@@ -0,0 +1,48 @@
+import { jikanGet, jikanUrl, queryJoin } from "./index";
+import urljoin from "url-join";
+import * as Types from "./types";
+
+export class Character {
+  /** @ignore */
+  private baseUrl: string;
+
+  constructor(id: number) {
+    this.baseUrl = `${jikanUrl}/characters/${id}`;
+  }
+
+  info(): Promise<Types.Character> {
+    return jikanGet(this.baseUrl);
+  }
+
+  anime(): Promise<Types.CharacterAnime> {
+    return jikanGet(urljoin(this.baseUrl, "anime"));
+  }
+
+  manga(): Promise<Types.CharacterManga> {
+    return jikanGet(urljoin(this.baseUrl, "manga"));
+  }
+
+  voiceActors(): Promise<Types.CharacterVoiceActors> {
+    return jikanGet(urljoin(this.baseUrl, "voices"));
+  }
+
+  pictures(): Promise<Types.CharacterPictures> {
+    return jikanGet(urljoin(this.baseUrl, "pictures"));
+  }
+}
+
+export function character(id: number): Character {
+  return new Character(id);
+}
+
+export function characterSearch(params?: {
+  page?: number;
+  limit?: number;
+  q?: string;
+  order_by?: "mal_id" | "name" | "favorites";
+  sort?: "desc" | "asc";
+  letter?: string;
+}): Promise<Types.CharactersSearch> {
+  const url = urljoin(jikanUrl, "characters") + queryJoin({ ...params });
+  return jikanGet(url);
+}
